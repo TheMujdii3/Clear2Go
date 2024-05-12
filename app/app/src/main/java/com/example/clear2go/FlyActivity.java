017786b26d05bab25eb8d60b9836707d1f93363f
TheMujdii3 <andreineculai70@gmail.com>
2024-05-12 12:58:53 +0300

    sequential order added

--- app/app/src/main/java/com/example/clear2go/FlyActivity.java
+++ app/app/src/main/java/com/example/clear2go/FlyActivity.java
@@ -76,70 +76,17 @@ public class FlyActivity extends AppCompatActivity implements LocationListener {
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
-
-
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseAuth.getCurrentUser();
         rq = mDatabase.getDatabase().getReference().child("Requests");
         avionData = mDatabase.getDatabase().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion);
+        
         binding.engOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rq.child(avion).child("Pornire motor").setValue(false);
                 binding.engOn.setBackgroundColor(Color.parseColor("#FFA500"));
-            }
-        });
-        binding.taxi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rq.child(avion).child("Rulaj").setValue(false);
-                binding.taxi.setBackgroundColor(Color.parseColor("#FFA500"));
-            }
-        });
-        binding.lineIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rq.child(avion).child("Intrare si aliniere").setValue(false);
-                binding.lineIn.setBackgroundColor(Color.parseColor("#FFA500"));
-            }
-        });
-        binding.Takeoff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rq.child(avion).child("Plecare").setValue(false);
-                binding.Takeoff.setBackgroundColor(Color.parseColor("#FFA500"));
-            }
-        });
-        binding.land.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //rq.child(avion).setValue("Aterizare "+avion);
-                rq.child(avion).child("Aterizare").setValue(false);
-                binding.land.setBackgroundColor(Color.parseColor("#FFA500"));
-            }
-        });
-        FirebaseDatabase.getInstance().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion + "/Rulaj").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    if (snapshot.getValue(boolean.class) == true) {
-                        binding.taxi.setBackgroundColor(-16711936);
-                        binding.taxi.setActivated(true);
-                        rulaj = true;
-                        //Toast.makeText(FlyActivity.this,"merge",Toast.LENGTH_SHORT).show();
-                    } else {
-                        binding.taxi.setBackgroundColor(-65536);
-                        binding.taxi.setActivated(false);
-                        rulaj = false;
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
         FirebaseDatabase.getInstance().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion + "/Pornire motor").addValueEventListener(new ValueEventListener() {
@@ -150,76 +97,164 @@ public class FlyActivity extends AppCompatActivity implements LocationListener {
                         binding.engOn.setBackgroundColor(-16711936);
                         binding.engOn.setActivated(true);
                         motor = true;
+                        binding.taxi.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                rq.child(avion).child("Rulaj").setValue(false);
+                                binding.taxi.setBackgroundColor(Color.parseColor("#FFA500"));
+                            }
+                        });
+                        FirebaseDatabase.getInstance().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion + "/Rulaj").addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists()) {
+                                    if (snapshot.getValue(boolean.class) == true) {
+                                        binding.taxi.setBackgroundColor(-16711936);
+                                        binding.taxi.setActivated(true);
+                                        rulaj = true;
+                                        binding.lineIn.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                rq.child(avion).child("Intrare si aliniere").setValue(false);
+                                                binding.lineIn.setBackgroundColor(Color.parseColor("#FFA500"));
+                                            }
+                                        });
+                                        FirebaseDatabase.getInstance().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion + "/Intrare si aliniere").addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                if (snapshot.exists()) {
+                                                    if (snapshot.getValue(boolean.class) == true) {
+                                                        binding.lineIn.setBackgroundColor(-16711936);
+                                                        binding.lineIn.setActivated(true);
+                                                        aliniat = true;
+                                                        binding.Takeoff.setOnClickListener(new View.OnClickListener() {
+                                                            @Override
+                                                            public void onClick(View v) {
+                                                                rq.child(avion).child("Plecare").setValue(false);
+                                                                binding.Takeoff.setBackgroundColor(Color.parseColor("#FFA500"));
+                                                            }
+                                                        });
+                                                        FirebaseDatabase.getInstance().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion + "/Plecare").addValueEventListener(new ValueEventListener() {
+                                                            @Override
+                                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                                if (snapshot.exists()) {
+                                                                    if (snapshot.getValue(boolean.class) == true) {
+                                                                        binding.Takeoff.setBackgroundColor(-16711936);
+                                                                        decolat = true;
+                                                                        binding.Takeoff.setActivated(true);
+                                                                        binding.land.setOnClickListener(new View.OnClickListener() {
+                                                                            @Override
+                                                                            public void onClick(View v) {
+                                                                                //rq.child(avion).setValue("Aterizare "+avion);
+                                                                                rq.child(avion).child("Aterizare").setValue(false);
+                                                                                binding.land.setBackgroundColor(Color.parseColor("#FFA500"));
+                                                                            }
+                                                                        });
+                                                                        FirebaseDatabase.getInstance().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion + "/Aterizare").addValueEventListener(new ValueEventListener() {
+                                                                            @Override
+                                                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                                                if (snapshot.exists()) {
+                                                                                    if (snapshot.getValue(boolean.class) == true) {
+                                                                                        binding.land.setBackgroundColor(-16711936);
+                                                                                        binding.land.setActivated(true);
+                                                                                        aterizat = true;
+                                                                                    } else {
+                                                                                        binding.land.setActivated(false);
+                                                                                        aterizat = false;
+                                                                                        binding.land.setBackgroundColor(-65536);
+                                                                                    }
+                                                                                }
+                                                                            }
+
+                                                                            @Override
+                                                                            public void onCancelled(@NonNull DatabaseError error) {
+
+                                                                            }
+                                                                        });
+                                                                    } else {
+                                                                        binding.Takeoff.setActivated(false);
+                                                                        binding.Takeoff.setBackgroundColor(-65536);
+                                                                        decolat = false;
+
+                                                                        binding.land.setActivated(false);
+                                                                        aterizat = false;
+                                                                        binding.land.setBackgroundColor(-65536);
+                                                                    }
+                                                                }
+                                                            }
+
+                                                            @Override
+                                                            public void onCancelled(@NonNull DatabaseError error) {
+
+                                                            }
+                                                        });
+                                                    } else {
+                                                        binding.lineIn.setActivated(false);
+                                                        aliniat = false;
+                                                        binding.lineIn.setBackgroundColor(-65536);
+
+                                                        binding.Takeoff.setActivated(false);
+                                                        binding.Takeoff.setBackgroundColor(-65536);
+                                                        decolat = false;
+
+                                                        binding.land.setActivated(false);
+                                                        aterizat = false;
+                                                        binding.land.setBackgroundColor(-65536);
+                                                    }
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+
+                                            }
+                                        });
+                                    } else {
+                                        binding.taxi.setBackgroundColor(-65536);
+                                        binding.taxi.setActivated(false);
+                                        rulaj = false;
+
+                                        binding.lineIn.setActivated(false);
+                                        aliniat = false;
+                                        binding.lineIn.setBackgroundColor(-65536);
+
+                                        binding.Takeoff.setActivated(false);
+                                        binding.Takeoff.setBackgroundColor(-65536);
+                                        decolat = false;
+
+                                        binding.land.setActivated(false);
+                                        aterizat = false;
+                                        binding.land.setBackgroundColor(-65536);
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
                     } else {
                         binding.engOn.setActivated(false);
                         motor = false;
                         binding.engOn.setBackgroundColor(-65536);
-                    }
-                }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                        binding.taxi.setBackgroundColor(-65536);
+                        binding.taxi.setActivated(false);
+                        rulaj = false;
 
-            }
-        });
-        FirebaseDatabase.getInstance().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion + "/Intrare si aliniere").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    if (snapshot.getValue(boolean.class) == true) {
-                        binding.lineIn.setBackgroundColor(-16711936);
-                        //Toast.makeText(FlyActivity.this,"merge",Toast.LENGTH_SHORT).show();
-                        binding.lineIn.setActivated(true);
-                        aliniat = true;
-                    } else {
                         binding.lineIn.setActivated(false);
                         aliniat = false;
                         binding.lineIn.setBackgroundColor(-65536);
-                    }
-                }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        FirebaseDatabase.getInstance().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion + "/Plecare").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    if (snapshot.getValue(boolean.class) == true) {
-                        binding.Takeoff.setBackgroundColor(-16711936);
-                        decolat = true;
-                        binding.Takeoff.setActivated(true);
-                        //Toast.makeText(FlyActivity.this,"merge",Toast.LENGTH_SHORT).show();
-                    } else {
                         binding.Takeoff.setActivated(false);
                         binding.Takeoff.setBackgroundColor(-65536);
                         decolat = false;
-                    }
-                }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        FirebaseDatabase.getInstance().getReference().child("Utilizare/Aviatie/Aerodromuri/AR_AT Bucuresti/Flota/Avioane/" + avion + "/Aterizare").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    if (snapshot.getValue(boolean.class) == true) {
-                        binding.land.setBackgroundColor(-16711936);
-                        binding.land.setActivated(true);
-                        aterizat = true;
-                        //Toast.makeText(FlyActivity.this,"merge",Toast.LENGTH_SHORT).show();
-                    } else {
                         binding.land.setActivated(false);
                         aterizat = false;
                         binding.land.setBackgroundColor(-65536);
+
                     }
                 }
             }
@@ -229,31 +264,6 @@ public class FlyActivity extends AppCompatActivity implements LocationListener {
 
             }
         });
-
-        /*
-        locationClient.getCurrentLocation(LocationRequest.QUALITY_HIGH_ACCURACY, new CancellationToken() {
-
-
-            @NonNull
-            @Override
-            public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
-                return null;
-            }
-
-            @Override
-            public boolean isCancellationRequested() {
-                return false;
-            }
-        }).addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                binding.alti.setText((int) location.getAltitude());
-            }
-        });
-
-         */
-
-
 
 
     }
