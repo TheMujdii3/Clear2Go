def44f63567153d01d4f0514530dc789f3ee6c2c
TheMujdii3 <andreineculai70@gmail.com>
2024-06-25 22:56:45 +0300

    one working window i hate UI

---
+++ C2G Desktop/Clear2GoDesk/src/main/java/test.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class test extends JFrame {
+    private JButton button1;
+    private JPanel panel1;
+    private JLabel pana;
+
+    public test() {
+        super("Simple Form");
+        panel1  = new JPanel();
+        button1 = new JButton();
+        pana = new JLabel();
+
+        panel1.add(button1);
+        panel1.add(pana);
+        setContentPane(panel1);
+        // ... (rest of the code)
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Your button click action here (e.g., display a message)
+                pana.setText("Button clicked!");
+            }
+        });
+    }
+    public static void main(String[] args) {
+        JFrame frame = new test();
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
+
