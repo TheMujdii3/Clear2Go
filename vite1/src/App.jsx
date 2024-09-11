8416b41b1fe5b076abf09b6b50a755cf70b0835b
TheMujdii3 <72468962+TheMujdii3@users.noreply.github.com>
2024-09-11 17:15:35 +0300

    no more private data

--- vite1/src/App.jsx
+++ vite1/src/App.jsx
@@ -4,16 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, set } from "firebase/database";
 import planeImage from './plane2.png';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyA51DFl-w_2WgBqe1KeginlkTFxlLVxKSY",
-    authDomain: "clear2go1.firebaseapp.com",
-    databaseURL: "https://clear2go1-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "clear2go1",
-    storageBucket: "clear2go1.appspot.com",
-    messagingSenderId: "341512314786",
-    appId: "1:341512314786:web:75b39fbb8298ac6a301cfb",
-    measurementId: "G-KRX68G6XWN"
-};
+
 
 function App() {
     const { isLoaded } = useLoadScript({
